60e3dea05136e2eaf627fd1d62185b8cde93ec62
David Davila Pino <daviddavilapino@gmail.com>
2017-12-22 21:55:20 +0100

    universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TransferState, makeStateKey, Meta, Title } from '@angular/platform-browser';
+
+const DOGS_KEY = makeStateKey('dogs');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+
+  dogs: any;
+
+  constructor(
+    private http: HttpClient,
+    private state: TransferState,
+    meta: Meta, 
+    title: Title
+  ) { 
+  	title.setTitle('Blogist');
+
+      // Sets the <meta> tag for the page
+	  meta.addTags([
+	    { name: 'author', content: 'Blogist' },
+	    { name: 'description', content: 'This is a description.' },
+	  ]);
+  }
+
+  ngOnInit() {
+    this.dogs = this.state.get(DOGS_KEY, null as any);
+
+    if (!this.dogs) {
+      this.http
+        .get('https://dog.ceo/api/breeds/list/all')
+        .subscribe(data => {
+          this.dogs = data;
+          this.state.set(DOGS_KEY, data as any);
+        });
+    }
+  }
 }
